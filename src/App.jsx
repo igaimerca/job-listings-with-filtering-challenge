bbf4fbd6cf710321df8b95b87cdfa55b6a1f8056
igaimerca <igaimerca@gmail.com>
2022-03-30 17:14:35 +0200

    feat: worked on filter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import FilterOptions from './components/FilterOptions/FilterOptions';
 import Header from './components/Header/Header';
 import JobCard from './components/JobCard/JobCard';
@@ -6,11 +6,26 @@ import { jobs } from './data';
 
 function App() {
 
+    let [data, setData] = useState(jobs);
+    let [filterOptions, setFilterOptions] = useState(["Senior"]);
+
+    useEffect(() => {
+        for (let i = 0; i < filterOptions.length; i++) {
+            setData(data.filter((job) => job.languages.includes(filterOptions[i]) || filterOptions[i] == job.role || filterOptions[i] == job.level))
+        }
+    }, [filterOptions])
+
     return (
         <div className="h-full">
             <Header />
-            <FilterOptions />
-            {jobs && jobs.map((job) => (
+            <div className="flex-wrap rounded-md w-9/12 bg-white flex items-center justify-start shadow-md h-auto p-[1.1rem] -mt-8 m-auto">
+                {filterOptions && filterOptions.map((option) => (
+                    <FilterOptions
+                        option={option}
+                        removeOption={(option) => setFilterOptions(filterOptions.filter((op) => op != option))} />
+                ))}
+            </div>
+            {data && data.map((job) => (
                 <JobCard
                     logo={job.logo}
                     position={job.position}
--- src/components/FilterOptions/FilterOptions.jsx
+++ src/components/FilterOptions/FilterOptions.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
-function FilterOptions() {
+function FilterOptions({removeOption, option}) {        
     return (
-        <div className="rounded-md w-9/12 bg-white flex items-center justify-start shadow-md h-auto p-[1.1rem] -mt-8 m-auto">
-            <ul className="filter-ul flex items-center justify-center light-grayish-cyan-bg">
+            <ul className="filter-ul flex items-center justify-center light-grayish-cyan-bg m-2">
                 <li className="m-0 hover:bg-transparent hover:text-black cursor-default">
-                    Frontend
+                    {option}
                 </li>
-                <li className="m-0 text-white bg-black rounded-t-md">
+                <li className="m-0 text-white bg-black rounded-none hover:rounded-none" onClick={() => removeOption(option)}>
                     X
                 </li>
             </ul>
-        </div>
     )
 }
 
