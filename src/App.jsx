66ba08093b7f462b08307c968933a687ccd09db6
igaimerca <igaimerca@gmail.com>
2022-03-30 16:05:39 +0200

    feat: added job logo and details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import Header from './components/Header/Header';
+import JobCard from './components/JobCard/JobCard';
+import { jobs } from './data';
 
 function App() {
 
     return (
         <div className="h-full">
             <Header />
+            {jobs && jobs.map((job) => (
+                <JobCard
+                    logo={job.logo}
+                    position={job.position}
+                    company={job.company}
+                    langauges={job.languages}
+                    role={job.role}
+                    level={job.level}
+                    contract={job.contract}
+                />
+            ))}
         </div>
     )
 }
