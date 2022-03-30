6e4d84d276e7e107c7ae20f0637a05503a36c4df
igaimerca <igaimerca@gmail.com>
2022-03-30 16:20:12 +0200

    feat: worked on responsiveness of the cards

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 
-function JobCard({position, company, langauges, role, level, contract, logo}) {
+function JobCard({
+  position,
+  company,
+  langauges,
+  role,
+  level,
+  contract,
+  logo,
+}) {
   return (
     <div className="w-9/12 my-7 m-auto job-card h-auto flex items-center flex-wrap justify-around py-7 px-3 rounded-md border-left">
       <div className="profile flex my-2">
         <img src={logo} alt="" />
       </div>
       <div className="job-details flex flex-col flex-[1] my-2 mx-3">
-        <div className="title flex my-1">
+        <div className="title flex my-1 flex-wrap">
           <h2 className="primary-color font-bold">{company}</h2>
-          <span className="badge primary-bg rounded-full px-2 pt-1 text-sm font-bold text-white">
+          <span className="badge primary-bg rounded-full px-2 pt-1 text-sm font-bold text-white new-badge">
             NEW!
           </span>
           <span className="badge badge very-dark-grayish-cyan-bg rounded-full px-2 pt-1 text-sm font-bold text-white">
             FEATURED
           </span>
         </div>
-        <h1 className="font-bold very-dark-grayish-cyan-color">
-         {position}
-        </h1>
+        <h1 className="font-bold very-dark-grayish-cyan-color">{position}</h1>
         <div className="metadata flex items-center justify-between">
           <small className="dark-grayish-cyan-color">1d ago</small>
           <div className="dot rounded-full dark-grayish-cyan-bg"></div>
@@ -28,11 +34,9 @@ function JobCard({position, company, langauges, role, level, contract, logo}) {
       </div>
       <div className="tech-stack flex justify-end flex-[2] mx-3 primary-color font-bold">
         <ul className="flex tech-stack-ul flex-wrap">
-            <li>{role}</li>
-            <li>{level}</li>
-          {langauges && langauges.map((langauge) => (
-              <li>{langauge}</li>
-          ))}
+          <li>{role}</li>
+          <li>{level}</li>
+          {langauges && langauges.map((langauge) => <li>{langauge}</li>)}
         </ul>
       </div>
     </div>
